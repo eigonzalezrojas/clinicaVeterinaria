8516b1ed0957d88de9dd7bed8f5b472a55c10db5
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2024-11-29 20:59:43 -0300

    create class Duenio

--- src/Duenio.java
+++ src/Duenio.java
@@ -1,2 +1,55 @@
 public class Duenio {
+    public String rut;
+    public String nombre;
+    public String direccion;
+    public String telefono;
+    public String correo;
+
+    public Duenio(String rut, String nombre, String direccion, String telefono, String correo) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
